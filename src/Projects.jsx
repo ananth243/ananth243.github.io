5fe09d47829f78c800352fc2d7ff48504f55a409
Ananth Raghav <raghavananth02@gmail.com>
2023-03-15 18:57:50 +0530

    Added svg animation

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,18 +1,25 @@
 // Photos from https://citizenofnowhe.re/lines-of-the-city
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll, useSpring } from "framer-motion";
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import Project from "./components/Project";
 import { projects } from "./util/exp";
+import AnimateName from "./components/AnimateName";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
   const footerBackground = useColorModeValue("dark.100", "light.100");
   const { scrollYProgress } = useScroll();
+  const [sign, setSign] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if(latest==1) setSign(true);
+    else setSign(false);
+  })
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
     restDelta: 0.001,
   });
-
   return (
     <Box
       as={motion.div}
@@ -35,6 +42,11 @@ export default function Projects() {
           bottom: "100px",
         }}
       />
+      {sign && (
+        <Box position={"fixed"} style={{ right: "5px", bottom: "20px" }}>
+          <AnimateName />
+        </Box>
+      )}
     </Box>
   );
 }
