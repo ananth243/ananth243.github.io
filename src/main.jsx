fae048642181b93d55c4e451829bddcee7d4bd50
Ananth Raghav <raghavananth02@gmail.com>
2023-03-15 11:58:06 +0530

    Removed routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CustomChakraProvider from "./config/CustomChakraProvider";
 import App from "./App";
-import './assets/styles/projects.css';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  }
-]);
+import "./assets/styles/projects.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CustomChakraProvider>
       <Navbar>
-        <RouterProvider router={router} />
+        <App />
       </Navbar>
     </CustomChakraProvider>
   </React.StrictMode>
